576251b89fb68cf19a5534bd3cf63d795e8ac3b0
gaobio.fr <ntt188@gmail.com>
2014-07-15 23:22:09 +0200

    Création du projet + Test avoiding null

---
+++ GuavaTest/src/test/java/com/guavatest/basicUtilities/AvoidingNullTest.java
@@ -0,0 +1,40 @@
+package com.guavatest.basicUtilities;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.base.Optional;
+
+public class AvoidingNullTest {
+
+	@Test(expected=NullPointerException.class)
+	public void ofNullTest() {
+		Optional.of(null);
+	}
+	
+	@Test
+	public void isPresentTest() {
+		Optional<Integer> possible = Optional.of(5);
+		Assert.assertTrue(possible.isPresent());
+	}
+	
+	@Test
+	public void absentTest() {
+		Optional<Integer> absent = Optional.absent();
+		Assert.assertFalse(absent.isPresent());
+	}
+	
+	@Test
+	public void fromNullablePresentTest() {
+		Integer intValue = new Integer(5);
+		Optional<Integer> present = Optional.fromNullable(intValue);
+		Assert.assertTrue(present.isPresent());
+	}
+	
+	@Test
+	public void fromNullableAbsentTest() {
+		Integer intValue = null;
+		Optional<Integer> present = Optional.fromNullable(intValue);
+		Assert.assertFalse(present.isPresent());
+	}
+}
