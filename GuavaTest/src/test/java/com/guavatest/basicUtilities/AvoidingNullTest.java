5303933a6ee481100878be3593b52ae42f7f898a
ntt188 <ntt188@gmail.com>
2014-08-19 11:41:00 +0200

    import static

--- GuavaTest/src/test/java/com/guavatest/basicUtilities/AvoidingNullTest.java
+++ GuavaTest/src/test/java/com/guavatest/basicUtilities/AvoidingNullTest.java
@@ -1,11 +1,15 @@
 package com.guavatest.basicUtilities;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.base.Optional;
@@ -20,106 +24,106 @@ public class AvoidingNullTest {
 	@Test
 	public void isPresentTest() {
 		Optional<Integer> possible = Optional.of(5);
-		Assert.assertTrue(possible.isPresent());
+		assertTrue(possible.isPresent());
 	}	
 	
 	@Test
 	public void absentTest() {
 		Optional<Integer> absent = Optional.absent();
-		Assert.assertFalse(absent.isPresent());
+		assertFalse(absent.isPresent());
 	}
 	
 	@Test
 	public void fromNullablePresentTest() {
 		Integer intValue = new Integer(5);
 		Optional<Integer> present = Optional.fromNullable(intValue);
-		Assert.assertTrue(present.isPresent());
+		assertTrue(present.isPresent());
 	}
 	
 	@Test
 	public void fromNullableAbsentTest() {
 		Integer intValue = null;
 		Optional<Integer> present = Optional.fromNullable(intValue);
-		Assert.assertFalse(present.isPresent());
+		assertFalse(present.isPresent());
 	}
 	
 	@Test
 	public void getTest() {
 		Optional<Integer> possible = Optional.of(5);
-		Assert.assertEquals(new Integer(5), possible.get());
+		assertEquals(new Integer(5), possible.get());
 	}
 	
 	@Test
 	public void orPresentTest() {
 		Optional<Integer> possible = Optional.of(5);
-		Assert.assertEquals(new Integer(5), possible.or(7));
+		assertEquals(new Integer(5), possible.or(7));
 	}
 	
 	@Test
 	public void orAbsentTest() {
 		Optional<Integer> possible = Optional.absent();
-		Assert.assertEquals(new Integer(7), possible.or(7));
+		assertEquals(new Integer(7), possible.or(7));
 	}
 	
 	@Test
 	public void orOptionalPresentTest() {
 		Optional<Integer> possible = Optional.of(5);
 		Optional<Integer> defaultOptional = Optional.of(7);
-		Assert.assertEquals(possible, possible.or(defaultOptional));
+		assertEquals(possible, possible.or(defaultOptional));
 	}
 	
 	@Test
 	public void orOptionnalAbsentTest() {
 		Optional<Integer> possible = Optional.absent();
 		Optional<Integer> defaultOptional = Optional.of(7);
-		Assert.assertEquals(defaultOptional, possible.or(defaultOptional));
+		assertEquals(defaultOptional, possible.or(defaultOptional));
 	}
 	
 	@Test
 	public void orNullPresentTest() {
 		Optional<Integer> possible = Optional.of(5);
-		Assert.assertEquals(new Integer(5), possible.orNull());
+		assertEquals(new Integer(5), possible.orNull());
 	}
 	
 	@Test
 	public void orNullAbsentTest() {
 		Optional<Integer> possible = Optional.absent();
-		Assert.assertNull(possible.orNull());
+		assertNull(possible.orNull());
 	}	
 	
 	@Test
 	public void asSetAbsentTest() {
 		Optional<Integer> possible = Optional.absent();
 		Set<Integer> set = possible.asSet();
-		Assert.assertTrue(set.isEmpty());
+		assertTrue(set.isEmpty());
 	}
 	
 	@Test
 	public void asSetPresentTest() {
 		Optional<Integer> possible = Optional.of(5);
 		Set<Integer> set = possible.asSet();
-		Assert.assertEquals(1, set.size());
-		Assert.assertEquals(new Integer(5), set.iterator().next());
+		assertEquals(1, set.size());
+		assertEquals(new Integer(5), set.iterator().next());
 	}
 	
 	@Test
 	public void equalsTest() {
-		Assert.assertTrue(Optional.of(5).equals(Optional.of(5)));
+		assertTrue(Optional.of(5).equals(Optional.of(5)));
 	}
 	
 	@Test
 	public void notEqualsTest() {
-		Assert.assertFalse(Optional.of(5).equals(Optional.of(4)));
+		assertFalse(Optional.of(5).equals(Optional.of(4)));
 	}
 	
 	@Test
 	public void equalsAbsentTest() {
-		Assert.assertTrue(Optional.absent().equals(Optional.absent()));
+		assertTrue(Optional.absent().equals(Optional.absent()));
 	}
 	
 	@Test
 	public void notEqualsAbsentTest() {
-		Assert.assertFalse(Optional.absent().equals(Optional.of(5)));
+		assertFalse(Optional.absent().equals(Optional.of(5)));
 	}
 	
 	@Test
@@ -131,7 +135,7 @@ public class AvoidingNullTest {
 		listValues.add(Optional.of(7));
 		
 		Iterator<Integer> iterPresents = Optional.presentInstances(listValues).iterator();
-		Assert.assertEquals(new Integer(5), iterPresents.next());
-		Assert.assertEquals(new Integer(7), iterPresents.next());
+		assertEquals(new Integer(5), iterPresents.next());
+		assertEquals(new Integer(7), iterPresents.next());
 	}
 }
